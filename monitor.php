3e1b1e4708ed4c61729f7fb6e211a79ba6add322
hattori-dev <48802790+hattori-dev@users.noreply.github.com>
2019-03-21 16:50:26 +0000

    Add files via upload

---
+++ monitor.php
@@ -0,0 +1,146 @@
+<?php
+$result="";
+if(isset($_POST["ip"]))
+{
+	if(($_POST["ip"]=="") || ($_POST["port"]=="") || ($_POST["port"]=="")) { $result = "Erro há campos em branco\n"; }
+	else{
+		$file = fopen("workers.txt","a");
+		$linha = $_POST["ip"] . ":" . $_POST["port"] . ":" . $_POST["soft"] . PHP_EOL;
+		fputs($file, $linha) ;
+		$result = "adicionado";
+	}
+	//die(print_r($_POST));
+}
+
+$workers = array();
+$jsVar=array();
+$file = fopen("workers.txt","r");
+while ($tempStr=fgets($file)){
+	$tempStr=rtrim($tempStr);
+	$tempW = explode(":", $tempStr);
+	array_push($workers, array( "IP" => $tempW[0], "PORT" => $tempW[1], "SOFT" => $tempW[2]));
+	array_push($jsVar, "['" .$tempW[0]. "'," .$tempW[1]. ",'" .$tempW[2] . "']");
+	
+};
+
+$tempStr=implode(",", $jsVar);
+$jsVarWorkers="[" . $tempStr . "]";
+fclose($file);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {font-family: Tahoma, Arial, sans-serif;font-size: 80%;background-color: rgb(240, 240, 240);}
+<?php 
+//a {color: rgb(44, 55, 66);}a:link {text-decoration: none;}a:visited {color: rgb(44, 55, 66);}a:hover {color: rgb(255, 153, 0);}a:active {color: rgb(204, 122, 0);}
+//.all {max-width:600px;margin: auto;}
+//.header {background-color: rgb(30, 30, 30);color: white;padding: 10px;font-weight: bold;margin: 0px;margin-bottom: 10px;}
+//.version {font-size: 75%;text-align: right;}
+//.links {padding: 7px;text-align: center;background-color: rgb(215, 215, 215);box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2), 0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);}
+// .letter {font-weight: bold;} h4 {background-color: rgb(0, 130, 130);color: white;padding: 10px;margin: 10px 0px;}
+//.flex-container {display: -webkit-flex;display: flex;}
+//.flex-item {width: 33%;margin: 3px;}
+//.motd-box {background-color: #ccc;padding: 0px 10px 5px 10px;margin-bottom: 10px;}
+//.motd-head {border-bottom: 1px solid #000;margin-bottom: 0.5em;padding: 0.5em 0em;font-weight: bold;}
+//.motd-body {overflow: hidden;}
+?>
+.data th, td {padding: 5px 12px;text-align: right;border-bottom: 1px solid #ccc;}
+.data th {text-align: center;}
+.data tr:nth-child(even) {background-color: #ddd;}
+.data tr:hover {background-color: #aaa;}
+.data th {background-color: #ccc;}
+.data table {width: 100%;max-width: 600px;}
+
+
+</style>
+<script>
+function HATTORI(item)
+{
+	//alert(item);
+	var xmlhttp = new XMLHttpRequest();
+	document.getElementById("S" + item[0]).innerHTML = "loading..."; 
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			var bb = this.responseText;
+			//alert (bb);
+			if((item[2]=="xmrig")||(item[2]=="stak")){
+				var myObj = JSON.parse(bb);
+				var iHTML = "<td id='S"+ item[0]+"'>" + item[2] + "</td>";
+				iHTML += "<td><a href='http://" + item[0] + ":" + item[1] + (item[2]=="stak" ? "/h" : "")+ "'>" + myObj.worker_id + "</a></td>";
+				iHTML += "<td align='right'>" + myObj.hashrate["highest"] + "</td>";
+				//iHTML += "<td align='right'>" + myObj.hashrate["total"][0] + "</td>";
+				//iHTML += "<td align='right'>" + myObj.hashrate["total"][1] + "</td>";
+				iHTML += "<td align='right'>" + myObj.hashrate["total"][2] + "</td>";
+				var threads = myObj.hashrate["threads"];
+				if(threads)	threads.forEach(function(item) {
+						iHTML += "<td align='right'>" + item[2] + "</td>";
+					});
+			}else{
+				iHTML = bb;
+			}
+			document.getElementById("D" + item[0]).innerHTML = iHTML; 
+
+		}
+	};
+	urlStr = "semi-proxy.php?ip=" + item[0] + "&port=" +  item[1] + "&soft=" + item[2];
+	//alert(urlStr);
+	xmlhttp.open("GET", urlStr, true);
+	xmlhttp.send();
+	
+
+}
+
+function NINJA()
+{
+	var workers = <?php echo $jsVarWorkers; ?> ;
+	//alert(workers);
+	workers.forEach(HATTORI);
+}
+
+//var time = new Date().getTime();
+
+function refresh() {
+	//location.href = "monitor.php";
+	NINJA();
+	setTimeout(refresh, 8000);
+	
+}
+
+setTimeout(refresh, 8000);
+
+</script>
+</head>
+<body onload="NINJA()">
+<h3>Monitor para pregui&ccedil;osos</h3>
+<?php
+if($result==""){
+?>
+	<form action="monitor.php" method="post">
+	IP: <input type="text" name="ip">&nbsp;&nbsp;&nbsp;
+	Port: <input type="text" name="port">&nbsp;&nbsp;&nbsp;
+	<select name="soft">
+	  <option value="xmrig">xmrig</option>
+	  <option value="stak">stak</option>
+	</select>
+	<input type="submit" name="submit" value="Adicionar">
+	</form>
+<?php
+} else { echo $result; }
+?>
+<hr><div class='data'>
+<table border=1>
+<tr><th>Software</th><th>WorkerID</th><th>Highest</th><th>Total</th><th colspan=10>Threads</th></tr>
+<?php 
+
+foreach($workers as $worker) {
+	echo "<tr id='D" . $worker["IP"] . "'><td id='S" . $worker["IP"] . "'>" . $worker["SOFT"] . "</td><td>" . $worker["IP"] . "</td><td></td><td></td><td></td><td></td></tr>";
+}
+?>
+</table>
+</div>
+</body>
+</html>
+
+
---
+++ semi-proxy.php
@@ -0,0 +1,94 @@
+<?php 
+// ************************************************
+// *  FILENAME : semi-proxy.php
+// *  (intermediario que vai recolher os dados)
+// ************************************************
+if($_GET)
+{
+	switch ($_GET["soft"]) {
+		case "xmrig": $url = $_GET["ip"] . ":" . $_GET["port"] . "/"; break;
+		case "stak": $url = $_GET["ip"] . ":" . $_GET["port"] . "/h"; break;
+	}
+	$tempObj->worker_id = $_GET["ip"];
+	
+	$ch = curl_init(); 
+	curl_setopt($ch, CURLOPT_URL, $url); 
+	curl_setopt($ch, CURLOPT_FAILONERROR, true);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$output = curl_exec($ch); 
+	
+	if (($output === false) || ($output=="")) {
+		//echo  "CURL Error: " . curl_error($ch);
+		$tempObj->worker_id = $_GET["ip"];
+		$tempObj->hashrate["highest"] = "ERRO! " . curl_error($ch);
+		$tempObj->hashrate["total"][] = "ERRO!";
+		$tempObj->hashrate["total"][] = "ERRO!";
+		$tempObj->hashrate["total"][] = "ERRO!";
+		$output = json_encode($tempObj);
+		die($output);
+	}
+	curl_close($ch); 
+	
+	switch ($_GET["soft"]) {
+		// running XMRIG *********************************************
+		case "xmrig":  
+			// que maravilha não é preciso fazer nada !!!!
+		break;
+		
+		// running XMR-STAK ******************************************
+		case "stak":  
+			if($output)
+			{
+				//tentar criar um JSON com os mesmos dados do xmrig (worker_id, hashrate[highest e totals[]])
+				libxml_use_internal_errors(true);
+				$doc = new DOMDocument();
+				@$doc->loadHTML($output);
+				$table = $doc->getElementsByTagName('table')->item(0);
+				//print_r($table);
+				if($table==false) {
+					$tempObj->worker_id = $_GET["ip"];
+					$tempObj->hashrate["highest"] = "ERRO! stak?" ;
+					$tempObj->hashrate["total"][] = "ERRO!";
+					$tempObj->hashrate["total"][] = "ERRO!";
+					$tempObj->hashrate["total"][] = "ERRO!";
+					$output = json_encode($tempObj);
+					die($output);
+				}
+				$hr = "";
+				$contador=0;
+				foreach($table->getElementsByTagName('tr') as $tr)
+				{
+					$th = $tr->getElementsByTagName('th');
+					switch ($th->item(0)->nodeValue) {
+						case "Thread ID" : // ignore
+							break;
+						case "Totals:" :
+							foreach($tr->getElementsByTagName('td') as $td)	{ $tempObj->hashrate["total"][] = $td->nodeValue; }
+							break;
+						case "Highest:" :
+							$tempObj->hashrate["highest"] = $tr->getElementsByTagName('td')->item(0)->nodeValue;
+							break;
+						default :
+							
+							foreach($tr->getElementsByTagName('td') as $td)	{ 
+								$tempObj->hashrate["threads"][$contador][] = $td->nodeValue; 
+							}
+							$contador++;
+							break;
+					}
+				}
+			}
+			else
+			{
+				$tempObj->hashrate["highest"] = "ERRO! Sem output!";
+				$tempObj->hashrate["total"][] = "ERRO!";
+				$tempObj->hashrate["total"][] = "ERRO!";
+				$tempObj->hashrate["total"][] = "ERRO!";
+				
+			}
+			$output = json_encode($tempObj);
+			break;
+	}
+	echo $output;
+}
+?>
