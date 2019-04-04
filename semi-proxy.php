d44f43f4676edaa06d943fc72473288910752cfa
hattori-dev <48802790+hattori-dev@users.noreply.github.com>
2019-04-04 12:33:43 +0100

    Update semi-proxy.php
    Removed HTML tag reader in favor of xmr-stak API/JSON

--- semi-proxy.php
+++ semi-proxy.php
@@ -5,13 +5,8 @@
 // ************************************************
 if($_GET)
 {
-	$url = $_GET["ip"] . ":" . $_GET["port"] . ($_GET["soft"]=="stak" ? "/h" : "/" );
-	/*
-	switch ($_GET["soft"]) {
-		case "xmrig": $url = $_GET["ip"] . ":" . $_GET["port"] . "/"; break;
-		case "stak": $url = $_GET["ip"] . ":" . $_GET["port"] . "/h"; break;
-	}
-	*/
+	$url = $_GET["ip"] . ":" . $_GET["port"] . ($_GET["soft"]=="stak" ? "/api.json" : "/" );
+
 	$tempObj = new \stdClass();
 	$tempObj->worker_id = $_GET["ip"];
 	
@@ -22,79 +17,16 @@ if($_GET)
 	$output = curl_exec($ch); 
 	
 	if (($output === false) || ($output=="")) {
-		//echo  "CURL Error: " . curl_error($ch);
 		$tempObj->worker_id = $_GET["id"];
-		$tempObj->hashrate["highest"] = "ERRO! " . curl_error($ch);
+		$tempObj->hashrate["highest"] = "ERRO!";
 		$tempObj->hashrate["total"][] = "ERRO!";
 		$tempObj->hashrate["total"][] = "ERRO!";
 		$tempObj->hashrate["total"][] = "ERRO!";
+		$tempObj->hashrate["threads"][0][2] = "ERRO! " . curl_error($ch);
 		$output = json_encode($tempObj);
 		die($output);
 	}
 	curl_close($ch); 
-	
-	switch ($_GET["soft"]) {
-		// running XMRIG *********************************************
-		case "xmrig":  
-			// que maravilha não é preciso fazer nada !!!!
-		break;
-		
-		// running XMR-STAK ******************************************
-		case "stak":  
-			if($output)
-			{
-				//tentar criar um JSON com os mesmos dados do xmrig (worker_id, hashrate[highest e totals[]])
-				//libxml_use_internal_errors(true);
-				$doc = new DOMDocument();
-				@$doc->loadHTML($output);
-				$table = $doc->getElementsByTagName('table')->item(0);
-				//print_r($table);
-				if($table==false) {
-					$tempObj->worker_id = $_GET["id"];
-					$tempObj->hashrate["highest"] = "ERRO! stak?" ;
-					$tempObj->hashrate["total"][] = "ERRO!";
-					$tempObj->hashrate["total"][] = "ERRO!";
-					$tempObj->hashrate["total"][] = "ERRO!";
-					$output = json_encode($tempObj);
-					die($output);
-				}
-				$hr = "";
-				$contador=0;
-				foreach($table->getElementsByTagName('tr') as $tr)
-				{
-					$th = $tr->getElementsByTagName('th');
-					switch ($th->item(0)->nodeValue) {
-						case "Thread ID" : // ignore
-							break;
-						case "H/s": // ignore
-							break;
-						case "Totals:" :
-							foreach($tr->getElementsByTagName('td') as $td)	{ $tempObj->hashrate["total"][] = $td->nodeValue; }
-							break;
-						case "Highest:" :
-							$tempObj->hashrate["highest"] = $tr->getElementsByTagName('td')->item(0)->nodeValue;
-							break;
-						default :
-							
-							foreach($tr->getElementsByTagName('td') as $td)	{ 
-								$tempObj->hashrate["threads"][$contador][] = $td->nodeValue; 
-							}
-							$contador++;
-							break;
-					}
-				}
-			}
-			else
-			{
-				$tempObj->hashrate["highest"] = "ERRO! Sem output!";
-				$tempObj->hashrate["total"][] = "ERRO!";
-				$tempObj->hashrate["total"][] = "ERRO!";
-				$tempObj->hashrate["total"][] = "ERRO!";
-				
-			}
-			$output = json_encode($tempObj);
-		break;
-	} //end switch ($_GET["soft"])
 	echo $output;
 }
 ?>
