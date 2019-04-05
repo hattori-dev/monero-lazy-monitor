6a6ecac5b4ff0d5c0338103503308deeb29f88f4
hattori-dev <48802790+hattori-dev@users.noreply.github.com>
2019-04-05 13:41:15 +0100

    update config
    read "workers.txt" and turn it into "config.json" to use with new index.php

---
+++ update.php
@@ -0,0 +1,19 @@
+<?php
+$workers = array();
+$file = fopen("workers.txt","r");
+while ($tempStr=fgets($file)){
+	$tempStr=rtrim($tempStr);
+	$tempW = explode(":", $tempStr);
+	array_push($workers, array( "id" => $tempW[0], "ip" => $tempW[1], "port" => $tempW[2], "soft" => $tempW[3], "alert" => 0));
+};
+fclose($file);
+
+$newConfigDataArr = array_merge(array('refresh'=>90),array('workers'=>$workers));
+
+$newConfigDataJSON = json_encode($newConfigDataArr, JSON_PRETTY_PRINT);
+echo "<hr><pre>";
+echo $newConfigDataJSON;
+echo "</pre><hr>";
+file_put_contents("aconfig.json", $newConfigDataJSON);
+echo "config.json criado";
+?>
