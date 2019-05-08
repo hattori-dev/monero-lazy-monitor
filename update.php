fe7c1a02e6b7daecedd54fbfb6aa52f4aa6d2ef1
hattori-dev <48802790+hattori-dev@users.noreply.github.com>
2019-05-08 10:40:28 +0100

    Update update.php

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ $file = fopen("workers.txt","r");
 while ($tempStr=fgets($file)){
 	$tempStr=rtrim($tempStr);
 	$tempW = explode(":", $tempStr);
-	array_push($workers, array( "id" => $tempW[0], "ip" => $tempW[1], "port" => $tempW[2], "soft" => $tempW[3], "alert" => 0));
+	array_push($workers, array( "id" => $tempW[0], "ip" => $tempW[1], "port" => $tempW[2], "soft" => $tempW[3], "alert" => 0, "xmrigtoken" => "", "stakuser" => "", "stakpass" => ""));
 };
 fclose($file);
 
@@ -15,5 +15,5 @@ echo "<hr><pre>";
 echo $newConfigDataJSON;
 echo "</pre><hr>";
 file_put_contents("config.json", $newConfigDataJSON);
-echo "config.json created";
+echo "config.json criado";
 ?>
